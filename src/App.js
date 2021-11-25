72aefc24d3df1388422c6c1f4d1c7f7ea29e991a
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-11-25 04:22:36 +0200

    nav var responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 function App() {
-  return <p>portfolio</p>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
