a26b14408727cd2262a0db72f4e40719d0f0f117
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2021-11-27 14:31:19 +0200

    my services is completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -16,22 +17,23 @@ function App() {
               value: 30,
               density: {
                 enable: true,
-                value_area: 900
-              }
+                value_area: 900,
+              },
             },
             shape: {
               type: "circle",
               stroke: {
                 width: 6,
-                color: "#f9ab00"
-              }
-            }
-          }
+                color: "#f9ab00",
+              },
+            },
+          },
         }}
       />
       <Navbar />
       <Header />
       <AboutMe />
+      <Services />
     </>
   );
 }
